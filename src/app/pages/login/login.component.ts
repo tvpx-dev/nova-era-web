73d0c3404f04863af84752123047276d3e677af7
tvpx-dev <tvpx-dev@gmail.com>
2025-12-28 15:13:37 -0300

    finaliza criacao de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TitlesSectionComponent } from '../../components/titles/titles-section/titles-section.component';
 
 @Component({
   selector: 'new-login',
-  imports: [],
+  imports: [TitlesSectionComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
